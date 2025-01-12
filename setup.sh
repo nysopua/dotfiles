d73f047bf0fc0f5a0b9d49e00ad7dd1454383757
nysopua <nysopua@gmail.com>
2025-01-12 20:11:42 +0900

    文言調整

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 echo "セットアップを開始します"
 
-# dotfilesのシンボリックリンクを作成する
+echo "[1/3] dotfilesのシンボリックリンクを作成しています..."
 source ~/dotfiles/link.sh
 
-# homebrewがインストールされていない場合はインストール
+echo "[2/3] Homebrewの確認とインストール..."
 if ! type brew >/dev/null 2>&1; then
   echo "Homebrewをインストールします"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -13,17 +13,13 @@ else
   echo "Homebrewはすでにインストールされています"
 fi
 
-# Brewfileに記載されているパッケージをインストール
+echo "[3/3] 必要なパッケージと環境設定を実行..."
 source ~/dotfiles/brew-init.sh
-
-# zshをbrewのものに置き換え
 source ~/dotfiles/replace-zsh.sh
-
-# gitをbrewのものに置き換え
 source ~/dotfiles/replace-git.sh
 
 echo "セットアップが完了しました"
-echo "Next..."
+echo "Next Steps:"
 echo "1. .envを追加し、環境変数を設定してください。"
 echo "2. git/user.confを追加し、ユーザー情報を設定してください。"
 echo "3. ターミナルを再起動してください。"
