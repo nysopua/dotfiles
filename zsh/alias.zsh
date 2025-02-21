00a7c705a392a03ddd46155dbf250fa8e3338e0d
nysopua <nysopua@gmail.com>
2025-02-22 07:05:59 +0900

    alias修正

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,7 +2,7 @@
 
 alias c='clear'
 alias reload='exec $SHELL -l'
-alias ls='eza -alh --icons=auto --no-user'
+alias ls='eza -a -i -h --icons=auto --no-user'
 alias cat='bat --theme="Dracula"'
 alias find='fd'
 alias cdf='cd "$(dirname "$(fzf --preview="bat --color=always {}")")"'
